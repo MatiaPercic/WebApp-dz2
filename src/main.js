567e2e7681b132b1d927c6250eaa91d61fb09cda
MatiaPercic <mapercic@unipu.hr>
2022-03-31 11:11:05 +0200

    app and main

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from './plugins/Vuetify'
 
-createApp(App).use(router).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+    router,
+    vuetify,
+    render: h => h(App)
+  }).$mount('#app')
